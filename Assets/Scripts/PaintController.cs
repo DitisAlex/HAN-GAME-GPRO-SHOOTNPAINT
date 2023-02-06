f279890587dba0d359cab85d26efa5039744bc0f
Alex Cheng <alexcheng0314@gmail.com>
2023-02-06 21:35:43 +0100

    Shoot N Paint Code (Assignment #1)

---
+++ Assets/Scripts/PaintController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintController : MonoBehaviour
+{
+    public LayerMask layerMask;
+    private float paintRange = 10;
+    private Color[] colors = new Color[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors[0] = Color.yellow;
+        colors[1] = Color.red;
+        colors[2] = Color.cyan;
+        colors[3] = Color.magenta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            Debug.DrawRay(ray.origin, ray.direction * paintRange, Color.blue, 0.5f);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, paintRange, layerMask))
+            {
+                if(hit.transform.tag == "Unpaintable")
+                {
+                    Debug.Log("Cube is unpaintable!");
+                } else
+                {
+                    Debug.Log("Painted the cube!");
+                    hit.collider.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float floatingDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, floatingDistance, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput();
+    }
+
+    void raycastMovement()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+
+        Debug.DrawRay(ray.origin, ray.direction * floatingDistance, Color.red, 0.5f);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, floatingDistance, layerMask))
+        {
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            if(hit.normal.y.Equals(1)){
+                transform.position = new Vector3(transform.position.x, floatingDistance, transform.position.z);
+            }
+        }
+    }
+
+    void movementInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            raycastMovement();
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+            raycastMovement();
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+    }
+}
